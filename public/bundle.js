aa1f473e1a2378c03d8425d89cd8393ff84a8ba7
itsandromeda <itsandromeda@gmail.com>
2017-07-07 21:15:10 -0500

    secondary-news desktop

--- public/bundle.js
+++ public/bundle.js
@@ -123,19 +123,20 @@ const secNews = (update) => {
         news1 = $('<img class="img-responsive" src="assets/img/news-1.png" alt="news-2">'),
         news2 = $('<img class="img-responsive" src="assets/img/news-2.png" alt="news-3">'),
         news3 = $('<img class="img-responsive" src="assets/img/news-3.png" alt="news-4">'),
-        title1 = $('<h3>1.	El método de la <span class="primary-color">Universidad de Stanford</span> para mejorar las calificaciones</h3>'),
+        title1 = $('<h3>El método de la <span class="primary-color">Universidad de Stanford</span> para mejorar las calificaciones</h3>'),
         title2 = $('<h3>Estas son las mejores <span class="primary-color">tech startups</span> del mundo</h3>'),
         title3 = $('<h3>Voluntarios por la <span class="primary-color">educación</span>, la combinación perfecta</h3>'),
-        info = $('<p class="hidden-xs hidden-sm">Ya no será necesario ir hasta el campus de MIT para estudiar allá, por medio de su curso gratuito en línea cualquiera podrá hacerlo.</p>');
+        info = $('<p class="hidden-xs hidden-sm">Ya no será necesario ir hasta el campus de MIT para estudiar allá, por medio de su curso gratuito en línea cualquiera podrá hacerlo.</p>'),
+        label = $('<div class="label">HOY</div>');
 
     container.append(wrap);
     wrap.append(row);
-    box_1.append(innerbox_1);
+    box_1.append(innerbox_1, label);
     box_2.append(innerbox_2);
     box_3.append(innerbox_3);
-    innerbox_1.append(news1);
-    innerbox_2.append(news2);
-    innerbox_3.append(news3);
+    innerbox_1.append(news1, title1);
+    innerbox_2.append(news2, title2);
+    innerbox_3.append(news3, title3);
     row.append(box_1, box_2, box_3);
 
     $(_ => {
